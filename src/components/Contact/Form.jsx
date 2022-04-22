c596b481717737767cc3eb987df57430adf3dd7e
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-04-22 02:14:41 -0300

    added form functionality

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,19 +1,65 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import './css/contact.css';
 
 const Form = () => {
    const { t } = useTranslation();
+   const [submitted, setSubmitted] = useState(false);
+   const FORM_ENDPOINT =
+      'https://public.herotofu.com/v1/30f346f0-c1f9-11ec-abfb-2b5c80ae2a8a';
+
+   const handleSubmit = () => {
+      setTimeout(() => {
+         setSubmitted(true);
+      }, 100);
+   };
+
+   if (submitted) {
+      return (
+         <>
+            <p className="form__submitted">{t('form_submitted')}</p>
+         </>
+      );
+   }
+
    return (
-      <form data-aos="slide-from-top" className="contact__form">
-         <input type="text" placeholder={t('full_name')} required />
+      <form
+         action={FORM_ENDPOINT}
+         onSubmit={handleSubmit}
+         method="POST"
+         target="_blank"
+         data-aos="slide-from-top"
+         className="contact__form"
+      >
+         <input
+            type="text"
+            id="name"
+            name="Name"
+            placeholder={t('full_name')}
+            required
+         />
          <div className="form__btn-wrapper">
-            <input type="email" placeholder="Email" required />
-            <input type="tel" placeholder={t('phone')} required />
+            <input
+               type="email"
+               id="email"
+               name="Email"
+               placeholder="Email"
+               required
+            />
+            <input
+               type="tel"
+               id="tel"
+               name="Tel"
+               placeholder={t('phone')}
+               required
+            />
          </div>
          <textarea
             placeholder={t('enter_message')}
             required
             rows="10"
+            id="message"
+            name="Message"
          ></textarea>
          <button type="submit">{t('submit')}</button>
       </form>
