8c739f2197ebd895c263447788d91c811af29b1f
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-04-22 15:28:58 -0300

    fixed bug

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
 
 import App from './views/App';
 import './reset.css';
@@ -11,10 +10,21 @@ import './index.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'flag-icon-css/css/flag-icons.min.css';
 
+import translationEN from '../assets/locales/en/translation.json';
+import translationPT from '../assets/locales/pt/translation.json';
+
+const resources = {
+   en: {
+      translation: translationEN,
+   },
+   pt: {
+      translation: translationPT,
+   },
+};
+
 i18n
    .use(initReactI18next)
    .use(LanguageDetector)
-   .use(HttpApi)
    .init({
       supportedLngs: ['pt', 'en'],
       fallbackLng: 'pt',
@@ -22,9 +32,7 @@ i18n
          order: ['cookie', 'localStorage', 'htmlTag', 'path', 'subdomain'],
          caches: ['cookie'],
       },
-      backend: {
-         loadPath: '/assets/locales/{{lng}}/translation.json',
-      },
+      resources,
    });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
