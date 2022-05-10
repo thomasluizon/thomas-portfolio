7d01a7bcd96afbe4d7c5eb9fdb37a6a9242433cc
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-05-10 15:29:56 -0300

    changed animation to only once and changed section height

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,25 +1,21 @@
-import { useTranslation } from 'react-i18next';
-
 const Project = props => {
-   const { t } = useTranslation();
-
-   return (
-      <div data-aos="slide-from-top" className="projects__project">
-         <img src={props.img} />
-         <div className="project__about">
-            <p>{props.desc}</p>
-            <div className="project__btns">
-               <a target="_blank" href={props.repo}>
-                  <i className="bi bi-github"></i>
-                  Repo
-               </a>
-               <a target="_blank" href={props.demo}>
-                  {t('demo')}
-               </a>
-            </div>
-         </div>
-      </div>
-   );
+	return (
+		<div data-aos="slide-from-top" className="projects__project">
+			<img src={props.img} />
+			<div className="project__about">
+				<p>{props.desc}</p>
+				<div className="project__btns">
+					<a target="_blank" href={props.repo}>
+						<i className="bi bi-github"></i>
+						Repo
+					</a>
+					<a target="_blank" href={props.demo}>
+						Demo
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Project;
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -9,35 +9,35 @@ import Projects from '../components/Projects/Projects';
 import Contact from '../components/Contact/Contact';
 
 const App = () => {
-   const languages = [
-      {
-         code: 'pt',
-         name: 'Português',
-         country_code: 'br',
-      },
-      {
-         code: 'en',
-         name: 'English',
-         country_code: 'us',
-      },
-   ];
+	const languages = [
+		{
+			code: 'pt',
+			name: 'Português',
+			country_code: 'br',
+		},
+		{
+			code: 'en',
+			name: 'English',
+			country_code: 'us',
+		},
+	];
 
-   useEffect(() => {
-      Aos.init({ duration: 2000 });
-   }, []);
+	useEffect(() => {
+		Aos.init({ duration: 2000, once: true });
+	}, []);
 
-   return (
-      <>
-         <Header languages={languages} />
-         <main>
-            <Home />
-            <About />
-            <Projects />
-            <Contact />
-         </main>
-         <Footer />
-      </>
-   );
+	return (
+		<>
+			<Header languages={languages} />
+			<main>
+				<Home />
+				<About />
+				<Projects />
+				<Contact />
+			</main>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
