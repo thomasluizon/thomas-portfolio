56c30a8a9249d807573eb587b7a1f71cd3f4d790
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-05-16 14:33:15 -0300

    added show more projects button and new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import Project from './Project';
+import { useState } from 'react';
 
 const Projects = () => {
 	const { t } = useTranslation();
+	const [showMore, setShowMore] = useState(false);
 
 	return (
 		<section id="projects">
@@ -12,30 +14,49 @@ const Projects = () => {
 				</h2>
 				<div className="projects__container">
 					<Project
-						repo="https://github.com/thomasluizon/react-calculator"
-						demo="https://react-calculator-thomas.vercel.app/"
-						desc={t('calculator_desc')}
-						img="https://github.com/thomasluizon/react-calculator/raw/main/src/assets/Screenshot.png"
-					/>
-					<Project
-						repo="https://github.com/thomasluizon/react-notes-app"
-						demo="https://react-notes-app-thomas.vercel.app/"
-						desc={t('notes_desc')}
-						img="https://github.com/thomasluizon/react-notes-app/raw/main/Screenshot.png"
-					/>
-					<Project
-						repo="https://github.com/thomasluizon/ip-address-tracker-react"
-						demo="https://ip-address-tracker-react-thomas.vercel.app/"
-						desc={t('ipaddress_desc')}
-						img="https://github.com/thomasluizon/ip-address-tracker-react/blob/main/src/assets/ScreenshotMobile.png?raw=true"
-					/>
-					<Project
 						repo="https://github.com/thomasluizon/url-shortening-api-react"
 						demo="https://url-shortening-api-react.vercel.app/"
 						desc={t('linkshortener_desc')}
 						img="https://github.com/thomasluizon/url-shortening-api-react/raw/main/src/assets/Screenshots/Screenshot_2-mobile.png"
 					/>
+					<Project
+						repo="https://github.com/thomasluizon/next-crud"
+						demo="https://next-crud-thomas.vercel.app/"
+						desc={t('nextcrud_desc')}
+						img="https://github.com/thomasluizon/next-crud/raw/main/src/Screenshot.png"
+					/>
+
+					{showMore && (
+						<>
+							<Project
+								repo="https://github.com/thomasluizon/react-calculator"
+								demo="https://react-calculator-thomas.vercel.app/"
+								desc={t('calculator_desc')}
+								img="https://github.com/thomasluizon/react-calculator/raw/main/src/assets/Screenshot.png"
+							/>
+							<Project
+								repo="https://github.com/thomasluizon/react-notes-app"
+								demo="https://react-notes-app-thomas.vercel.app/"
+								desc={t('notes_desc')}
+								img="https://github.com/thomasluizon/react-notes-app/raw/main/Screenshot.png"
+							/>
+							<Project
+								repo="https://github.com/thomasluizon/ip-address-tracker-react"
+								demo="https://ip-address-tracker-react-thomas.vercel.app/"
+								desc={t('ipaddress_desc')}
+								img="https://github.com/thomasluizon/ip-address-tracker-react/blob/main/src/assets/ScreenshotMobile.png?raw=true"
+							/>
+						</>
+					)}
 				</div>
+
+				<button
+					className="show-more"
+					data-aos="slide-from-top"
+					onClick={() => setShowMore(!showMore)}
+				>
+					{t(showMore ? 'show_less' : 'show_more')}
+				</button>
 			</div>
 		</section>
 	);
