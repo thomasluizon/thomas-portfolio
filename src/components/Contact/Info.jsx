3fd340c1180e57cd52c17d475d7d27d634a4cdce
thomas-luizon <thomaslrgregorio@gmail.com>
2023-05-10 22:51:38 -0300

    tirei o desenho merda

--- src/components/Contact/Info.jsx
+++ src/components/Contact/Info.jsx
@@ -2,6 +2,8 @@ import { useTranslation } from 'react-i18next'
 
 const Info = () => {
 	const { t } = useTranslation()
+	const email = 'thomaslrgregorio@gmail.com'
+	const phone = '+5511942670844'
 	return (
 		<ul data-aos="slide-from-top" className="contact__info">
 			<li>
@@ -10,13 +12,11 @@ const Info = () => {
 			</li>
 			<li>
 				<i className="bi bi-envelope-fill"></i>
-				<a href="mailto:thomaslrgregorio@gmail.com">
-					thomaslrgregorio@gmail.com
-				</a>
+				<a href={`mailto:${email}`}>{email}</a>
 			</li>
 			<li>
 				<i className="bi bi-telephone-fill"></i>
-				<a href="tel:+5511942670844">+5511942670844</a>
+				<a href={`tel:${phone}`}>{phone}</a>
 			</li>
 		</ul>
 	)
